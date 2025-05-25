9026f1ae4a792597a110a20f07bb60c41ccd889a
Jorge Eastman <jeastman19@gmail.com>
2025-05-24 23:41:00 -0300

    Script de base de datos

---
+++ postgres/create-correos-dikigo-table.sql
@@ -0,0 +1,29 @@
+CREATE TABLE if not exists correos_dikigo (
+    email_id            VARCHAR(100) PRIMARY KEY,
+    fecha_hora          TIMESTAMP,
+    from_email          VARCHAR(50),
+    subject             TEXT,
+    body_raw            TEXT,
+    body_clean          TEXT,
+    intent_extraido     TEXT,
+	doc_id				varchar(100),
+    pregunta_original   VARCHAR(100),
+    question_vector_id  BIGINT,
+    score_top1          REAL,
+    question_id_top1    BIGINT,
+    respuesta_sugerida  TEXT,
+    respuesta_correcta  TEXT,
+    tipo_error          TEXT,
+    notas               TEXT,
+    pregunta			VARCHAR(2),
+    encontrado          VARCHAR(2),
+	procesado			VARCHAR(2),
+    correcto            VARCHAR(2)
+);
+
+CREATE INDEX IF NOT EXISTS idx_correos_dikigo_fecha_hora ON correos_dikigo(fecha_hora);
+
+CREATE INDEX IF NOT EXISTS idx_correos_dikigo_pregunta ON correos_dikigo(pregunta);
+CREATE INDEX IF NOT EXISTS idx_correos_dikigo_encontrado ON correos_dikigo(encontrado);
+CREATE INDEX IF NOT EXISTS idx_correos_dikigo_procesado ON correos_dikigo(procesado);
+CREATE INDEX IF NOT EXISTS idx_correos_dikigo_correcto ON correos_dikigo(correcto);
---
+++ postgres/marcar-preguntas.sql
@@ -0,0 +1,72 @@
+-- ¿Cómo obtener el título automotor?
+update	correos_dikigo
+		set		doc_id = '1Hu2vkQoDXAoCTAb6Br8Uhq8pIA0cTdKt1lFCi4SX3no',
+				pregunta_original = '¿Cómo obtener el título automotor?'
+		where	email_id in (
+                    '1965a0795a97eb97',
+                    '194a8be24d6fc2ae',
+                    '19484d956d04320b',
+                    '193ad1be77c23071',
+                    '18ea349f52beaee2'
+		);
+
+-- ¿Cómo obtener o renovar la Cédula Verde?
+update	correos_dikigo
+		set		doc_id = '1eJj7qipleqptfhwPiqZRsyY1Rd2M1NRlbx-hV-Eo0L0',
+				pregunta_original = '¿Cómo obtener o renovar la Cédula Verde?'
+		where	email_id in (
+                    '196d49fe288388e3',
+                    '196b58e1ce86255f',
+                    '196a04dd96194157',
+                    '19662e4fd6cf6fb8',
+                    '195ed3a1eb60a2c1',
+                    '1959afc71acd8969',
+                    '1953889be4fa80df',
+                    '195378a4b391a961',
+                    '194fe1369d4e2129',
+                    '1946a53abbeacc4d',
+                    '1946049277318cc5',
+                    '1945fd6fde3109f4',
+                    '1944de12901dab2a',
+                    '1943e8ae71f91506',
+                    '193e554728f9e607',
+                    '193b251515675b58',
+                    '192ed9f262eb0c46',
+                    '1920f7274b10383d',
+                    '1919fdf1bb5705af',
+                    '18fb51922e782d45',
+                    '18f96abf8e76bdaf',
+                    '18e38bd9e1b4d66e',
+                    '18e0baa01fa75048',
+                    '18d4b0772f0b767c'
+		);
+
+
+-- ¿Dónde y cómo puedo consultar las deudas de patente de mi vehículo?
+update	correos_dikigo
+		set		doc_id = '1-GuDaDqhLXSaLuzMwvahCD0qG9uQUhgptn6lfDnOGWQ',
+				pregunta_original = '¿Dónde y cómo puedo consultar las deudas de patente de mi vehículo?'
+		where	email_id in (
+            '18eae49e030377ee',
+            '18e5a0e8a5b709a3',
+            '18e58f72ffde5694',
+            '18e528e6a3d2d157',
+            '18dad369c405b0d6',
+            '18d581c0a0625c2c',
+            '18d5755efd38117a',
+            '18c5f7f9393fe6d0'
+		);
+
+-- ¿Cómo puedo iniciar el trámite de denuncia de venta de mi vehículo?
+
+-- ¿Qué pasos debo seguir para reponer o renovar la chapa patente de mi vehículo?
+update	correos_dikigo
+		set		doc_id = '1BOhr391pKCvomYixkr10vrtTPZjXVZo8fJXs5inQLkU',
+				pregunta_original = '¿Qué pasos debo seguir para reponer o renovar la chapa patente de mi vehículo?'
+		where	email_id in (
+                    '196408d728987186',
+                    '192f776faef86393',
+                    '18ea9c23529e4529'
+		);
+
+
